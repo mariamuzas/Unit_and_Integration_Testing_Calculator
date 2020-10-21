6dda00772c5a1ad4bd6df6f912af106cc7655d44
Your Name <mmuzasantuna@gmail.com>
2020-10-21 20:30:51 +0100

    tests for add() and subtract() added and passed

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -3,10 +3,29 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('App.vue', () => {
+  let wrapper; 
+
+  beforeEach( () => {
+    wrapper = shallowMount(App)
+  }),
+
   it('enterNum changes running total', () => {
-    const wrapper = shallowMount(App)
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
   })
+
+  it('should add', () => {
+    wrapper.vm.previousTotal = 1
+    wrapper.vm.add('4');
+    expect(wrapper.vm.runningTotal).to.equal(5)
+  })
+
+  it('should subtract', () => {
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+  
 })
