6aa79cc56c33c0ba57d25861e2595193c205b613
Your Name <mmuzasantuna@gmail.com>
2020-10-21 20:33:53 +0100

    tests for multiply() and divide() added and passed

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -13,19 +13,31 @@ describe('App.vue', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
-  })
+  });
 
   it('should add', () => {
     wrapper.vm.previousTotal = 1
     wrapper.vm.add('4');
     expect(wrapper.vm.runningTotal).to.equal(5)
-  })
+  });
 
   it('should subtract', () => {
     wrapper.vm.previousTotal = 7
     wrapper.vm.subtract('4');
     expect(wrapper.vm.runningTotal).to.equal(3)
-  })
+  });
 
-  
+  it('should multiply', () => {
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  });
+
+  it('should divide', () => {
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  });
+
+
 })
